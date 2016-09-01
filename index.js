4f6d205e9358e38a9044fcf642626fd02993be01
lupengyu <lupengyu@baidu.com>
2016-09-01 20:55:11 +0800

    fix import statement problem

--- index.js
+++ index.js
@@ -11,22 +11,45 @@ module.exports = function plugin(babel) {
 
     var t = babel.types;
 
+    var needToInsert = false;
+
     return {
 
         visitor: {
 
+            ImportSpecifier: function () {
+                needToInsert = true;
+            },
+
+            ImportDefaultSpecifier: function () {
+                needToInsert = true;
+            },
+
+            ImportNamespaceSpecifier: function () {
+                needToInsert = true;
+            },
+
             Program: {
+
+                enter: function () {
+                    console.log('program enter');
+                    needToInsert = false;
+                },
+
                 exit: function (path, state) {
+
+                    console.log('program exit');
 
                     var file = state.file;
                     var metadata = file.get('helpersNamespace');
 
 
-                    if (!metadata || !metadata.name) {
-                        return;
-                    }
-
-                    if (!file.usedHelpers || !Object.keys(file.usedHelpers).length) {
+                    if (
+                        // 没有使用 external helpers 插件
+                        !metadata || !metadata.name
+                        // 没有使用到任何 helpers 并且也没有使用 import
+                        || !(file.usedHelpers && Object.keys(file.usedHelpers).length) && !needToInsert
+                    ) {
                         return;
                     }
 
